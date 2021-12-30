afa23b36fa84b683d79fab9ecea789afac8d37d4
DaniOP30 <68250690+DaniOP30@users.noreply.github.com>
2021-12-30 02:11:54 -0600

    Categories Design

---
+++ src/components/categories.js
@@ -0,0 +1,8 @@
+import { Box, Button, GridItem, Heading, HStack, Image, SimpleGrid, Stack, Text, Tag} from "@chakra-ui/react";
+function Categories(){
+   return <Box>
+        Hola mundo
+    </Box>
+}
+
+export default Categories;
